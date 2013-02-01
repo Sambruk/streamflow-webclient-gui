5f6a15ab2156a8e024ecb0387de3332bb460b07c
Fredrik Frodlund <frippz@me.com>
2013-02-01 12:47:10 +0100

    Add positioning instead of margin (works more reliably in IE)

--- gui/js/static-prototype.js
+++ gui/js/static-prototype.js
@@ -5,13 +5,13 @@ $(function () {
     
     var slideMenu = $(".functions-menu");
     
-    $(slideMenu).css("margin-left", "-"+slideLength+"px");
+    $(slideMenu).css("left", "-"+slideLength+"px");
     
     $(".open-toolbar").on("click", function(e){
-        if ( $(slideMenu).css("margin-left") == "-"+slideLength+"px" ) {
-            $(slideMenu).animate({ "margin-left" : 0 }, 200)
+        if ( $(slideMenu).css("left") == "-"+slideLength+"px" ) {
+            $(slideMenu).animate({ "left" : 0 }, 200)
         } else {
-            $(slideMenu).animate({ "margin-left" : "-200" }, 200)
+            $(slideMenu).animate({ "left" : "-200" }, 200)
         };
         e.preventDefault();
     });
