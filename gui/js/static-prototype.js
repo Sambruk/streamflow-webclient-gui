1b7adf6771097e8cabecd3bb81556997962da8bc
Fredrik Frodlund <fredrik.frodlund@jayway.com>
2013-02-05 11:46:32 +0100

    Refactor: slide-in menu renamed to navigation

--- gui/js/static-prototype.js
+++ gui/js/static-prototype.js
@@ -1,29 +1,33 @@
 // Prototype specific startup scripts
 $(function () {
     
-    // Slide-in sidebar menu
+    // Slide-in navigation menu
     var slideLength = $(".sub-category").outerWidth();
-    
+            
     var slideMenu = $(".functions-menu");
-    
+            
     slideMenu.css("left", "-"+slideLength+"px");
-    
+            
     $(".open-toolbar").on("click", function(e) {
-        if (slideMenu.css("left") == "-"+slideLength+"px")
+        if (slideMenu.css("left") == "-"+slideLength+"px") {
             slideMenu.animate({ "left" : 0 }, 200)
-        else
+        } else {
             slideMenu.animate({ "left" : "-200" }, 200)
+        }
         e.preventDefault();
     });
     
     // Invoke conversations modal window
-    $(".modal-window").easyModal({
+    var modalWindow = $(".modal-window");
+    
+    modalWindow.easyModal({
         overlayOpacity: 0.7,
         overlayColor: "#000",
-        top: 150
+        top: 150,
+        closeButtonClass: "modal-close"
     });
     $(".m-convos .block-link").on("click", function(e) {
-        $('.modal-window').trigger('openModal');
+        modalWindow.trigger('openModal');
         e.preventDefault();
     });
     
