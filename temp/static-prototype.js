9e60c7c482d4d756012ce44ffd12c8b4d9778ec0
Fredrik Frodlund <frippz@me.com>
2013-07-24 11:38:08 +0200

    Fix better comment blocks and move Google Maps script into its own file

--- temp/static-prototype.js
+++ temp/static-prototype.js
@@ -1,7 +1,13 @@
-// Prototype specific startup scripts
+/**
+ * Prototype specific startup scripts
+ *
+ * @author Fredrik Frodlund
+ */
 $(function() {
     
-    // Event delegation for links
+    /**
+     * Event delegation for links
+     */
     $('body').on("click", function(e) {
         try {
             // Display an alert dialog when inactive links are clicked.
@@ -13,40 +19,61 @@ $(function() {
     });
     
     
-    // Slide in navigation
+    /**
+     * Slide in navigation
+     */
     $(".open-toolbar").on("click", function(e) {
         $(".functions-menu").toggleClass("open");
         e.preventDefault();
     });
     
     
-    // Custom select elements
+    /**
+     * Custom select elements
+     */
     $("select").customSelect({customClass:'custom-select'});
     
     
-    // Custom file upload elements
+    /**
+     * Custom file upload elements
+     */
     $("input:file").customFileInput();
     
     
-    // Placeholder support for legacy IE
+    /**
+     * Placeholder support for legacy IE
+     */
     $("input, textarea").placeholder();
     
     
-    // Expandable fields (TODO: Template this)
-    $(".expandable-field").addClass("active").append("<a href=\"#\" class=\"expand\"><i class=\"glyph icon-arrow-down\"></i><span>Visa mer</span><i class=\"glyph icon-arrow-down\"></i></a>").children(".expandable-content").hide();
+    /**
+     * Expandable fields
+     * TODO: Template this
+     */
+    $(".expandable-field")
+        .addClass("active")
+        .append("<a href=\"#\" class=\"expand\"><i class=\"glyph icon-arrow-down\"></i><span>Visa mer</span><i class=\"glyph icon-arrow-down\"></i></a>")
+        .children(".expandable-content")
+        .hide();
     $(".expand").on("click", function(e) {
         $(this).toggleClass("expanded").prevAll(".expandable-content").slideToggle("fast");
         e.preventDefault();
     });
     
-    // Date picker
+    
+    /**
+     * Date picker
+     */
     try {
         $('.datepicker').pickadate({
             format: 'yyyy-mm-dd',
         });
     } catch(e) {}
     
-    // Fix toolbar on scroll
+    
+    /**
+     * Fix toolbar on scroll
+     */
     var sidebar     = $(".toolbar"), 
         view        = $(window),
         offset      = sidebar.offset();
@@ -62,74 +89,23 @@ $(function() {
         } catch(e) {}
     });
     
-    // Google Maps config
-    function initMaps() {
-        $("#issues-map").gmap3({
-            map: {
-                options: {
-                    center: [55.603466,12.99416],
-                    zoom: 13,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    mapTypeControl: false,
-                    navigationControl: true,
-                    scrollwheel: false,
-                    streetViewControl: false
-                }
-            },
-            marker: {
-                values: [
-                    {latLng: [55.611375,12.974193]},
-                    {latLng: [55.604975,12.965953]},
-                    {latLng: [55.60032,12.986209]},
-                    {latLng: [55.617143,12.994939]},
-                    {latLng: [55.594464,12.992965]},
-                    {latLng: [55.594404,13.007062]},
-                    {latLng: [55.599859,13.004294]},
-                    {latLng: [55.597107,12.967451]},
-                    {latLng: [55.590605,12.967976]},
-                    {latLng: [55.590636,12.989111]},
-                    {latLng: [55.588192,12.997727]}
-                ],
-                options:{
-                    draggable: false,
-                    icon: new google.maps.MarkerImage(
-                        "gui/i/map-marker-green.png",
-                        new google.maps.Size(32, 37, "px", "px")
-                    )
-                }
-            }
-        });
-    }
     
-    // Superfish dropdown menu
+    /**
+     * Superfish dropdown menu
+     */
     try {
         $(".buttons ul").superfish();
     } catch (e) {}
     
-    // Toolbar toggle content
+    
+    /**
+     * Toolbar toggle content
+     */
     var toolbarContent = $(".toolbar-content");
     toolbarContent.hide();
     
     $(".buttons a").on("click", function(e){
         toolbarContent.slideToggle("fast");
-        e.preventDefault();
-    });
-    
-    // Google Maps toggle
-    if ($("body").hasClass("public")) {
-        var overviewHeading = $(".issues-overview > h2");
-        overviewHeading.append("<a href=\"#\" class=\"toggle-map\">Visa ärenden på karta</a>");
-        overviewHeading.after("<div id=\"issues-map\" class=\"map\"></div>");
-    }
-    
-    var toggleMap = $(".toggle-map");
-    var googleMap = $("#issues-map");
-    
-    googleMap.hide();
-    
-    toggleMap.on("click", function(e){
-        googleMap.slideToggle();
-        initMaps();
         e.preventDefault();
     });
     
