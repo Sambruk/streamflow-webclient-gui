8d8f3d4671926c47167fb4ad166f583bb696f433
Fredrik Frodlund <fredrik.frodlund@jayway.com>
2013-10-10 15:51:23 +0200

    Fix indendation
    Also, that's not how we write gradient syntax…

--- temp/static-prototype.js
+++ temp/static-prototype.js
@@ -4,120 +4,120 @@
  * @author Fredrik Frodlund
  */
 $(function() {
-    
-    /**
-     * Event delegation for links
-     */
-    $('body').on('click', function(e) {
-        try {
-            // Display an alert dialog when inactive links are clicked.
-            if ($(e.target).closest('a').attr('href').match(/(^\/$|\/?xlink-)/)) {
-                e.preventDefault();
-                alert('Denna länk är inte aktiv i prototypen.');
-            }
-        } catch(e) {}
-    });
-    
-    
-    /**
-     * Slide in navigation
-     */
-    $('.open-toolbar').on('click', function(e) {
-        $('.functions-menu').toggleClass('open');
-        e.preventDefault();
-    });
-    
-    
-    /**
-     * Custom select elements
-     */
-    $('.cust-sel').customSelect({customClass:'custom-select'});
-    
-    
-    /**
-     * Custom file upload elements
-     */
-    $('input:file').customFileInput();
-    
-    
-    /**
-     * Placeholder support for legacy IE
-     */
-    $('input, textarea').placeholder();
-    
-    
-    /**
-     * Expandable fields
-     * TODO: Find a templating system
-     */
-    $('.expandable-field')
-        .addClass('active')
-        .append('<a href="#" class="expand"><i class="glyph icon-arrow-down"></i><span>Visa mer</span><i class="glyph icon-arrow-down"></i></a>')
-        .children('.expandable-content')
-        .hide();
-    $('.expand').on('click', function(e) {
-        $(this).toggleClass('expanded').prevAll('.expandable-content').slideToggle('fast');
-        e.preventDefault();
-    });
-    
-    
-    /**
-     * Date picker
-     */
+  
+  /**
+  * Event delegation for links
+  */
+  $('body').on('click', function(e) {
     try {
-        $('.datepicker').pickadate({
-            format: 'yyyy-mm-dd',
-        });
+      // Display an alert dialog when inactive links are clicked.
+      if ($(e.target).closest('a').attr('href').match(/(^\/$|\/?xlink-)/)) {
+        e.preventDefault();
+        alert('Denna länk är inte aktiv i prototypen.');
+      }
     } catch(e) {}
-    
-    
-    /**
-     * Fix toolbar on scroll
-     */
-    var sidebar     = $('.toolbar'), 
-        view        = $(window),
-        offset      = sidebar.offset();
-
-    view.scroll(function() {
-        var contentSecondaryWidth = $('.content-secondary').width();
-        try {
-            if (view.scrollTop() > offset.top - 76) {
-                sidebar.addClass('fixed').css('width', contentSecondaryWidth);
-            } else {
-                sidebar.removeClass('fixed').removeAttr('style');
-            }
-        } catch(e) {}
+  });
+  
+  
+  /**
+  * Slide in navigation
+  */
+  $('.open-toolbar').on('click', function(e) {
+    $('.functions-menu').toggleClass('open');
+    e.preventDefault();
+  });
+  
+  
+  /**
+  * Custom select elements
+  */
+  $('.cust-sel').customSelect({customClass:'custom-select'});
+  
+  
+  /**
+  * Custom file upload elements
+  */
+  $('input:file').customFileInput();
+  
+  
+  /**
+  * Placeholder support for legacy IE
+  */
+  $('input, textarea').placeholder();
+  
+  
+  /**
+  * Expandable fields
+  * TODO: Find a templating system
+  */
+  $('.expandable-field')
+  .addClass('active')
+  .append('<a href="#" class="expand"><i class="glyph icon-arrow-down"></i><span>Visa mer</span><i class="glyph icon-arrow-down"></i></a>')
+  .children('.expandable-content')
+  .hide();
+  $('.expand').on('click', function(e) {
+    $(this).toggleClass('expanded').prevAll('.expandable-content').slideToggle('fast');
+    e.preventDefault();
+  });
+  
+  
+  /**
+  * Date picker
+  */
+  try {
+    $('.datepicker').pickadate({
+      format: 'yyyy-mm-dd',
     });
-    
-    
-    /**
-     * Superfish dropdown menu
-     */
+  } catch(e) {}
+  
+  
+  /**
+  * Fix toolbar on scroll
+  */
+  var sidebar     = $('.toolbar'), 
+  view        = $(window),
+  offset      = sidebar.offset();
+  
+  view.scroll(function() {
+    var contentSecondaryWidth = $('.content-secondary').width();
     try {
-        $('.buttons ul').superfish();
-    } catch (e) {}
-    
-    /**
-     * Accordion
-     */
-    $('.accordion-content').hide();
-    
-    $('.accordion .open-acc').on('click', function(e){
-        $('.accordion-content').slideUp('fast');
-        $(this).parent().siblings().slideToggle('fast');
-        e.preventDefault();
-    });
-    
-    
-    /**
-     * Toolbar toggle content
-     */
-    var toolbarContent = $('.toolbar-content');
-    toolbarContent.hide();
-    
-    $('.buttons a').on('click', function(e){
-        toolbarContent.slideToggle('fast');
-        e.preventDefault();
-    });
-    
+      if (view.scrollTop() > offset.top - 76) {
+        sidebar.addClass('fixed').css('width', contentSecondaryWidth);
+      } else {
+        sidebar.removeClass('fixed').removeAttr('style');
+      }
+    } catch(e) {}
+  });
+  
+  
+  /**
+  * Superfish dropdown menu
+  */
+  try {
+    $('.buttons ul').superfish();
+  } catch (e) {}
+  
+  /**
+  * Accordion
+  */
+  $('.accordion-content').hide();
+  
+  $('.accordion .open-acc').on('click', function(e){
+    $('.accordion-content').slideUp('fast');
+    $(this).parent().siblings().slideToggle('fast');
+    e.preventDefault();
+  });
+  
+  
+  /**
+  * Toolbar toggle content
+  */
+  var toolbarContent = $('.toolbar-content');
+  toolbarContent.hide();
+  
+  $('.buttons a').on('click', function(e){
+    toolbarContent.slideToggle('fast');
+    e.preventDefault();
+  });
+  
 });
